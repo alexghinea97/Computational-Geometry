d95a296354a818d4b4d5d3f66a59639ca2cdc1d1
Alex Ghinea <alex.ghinea@equilobe.com>
2018-01-07 00:29:01 +0200

    create graphics

--- Proiect GC/Form1.cs
+++ Proiect GC/Form1.cs
@@ -13,6 +13,7 @@ namespace Proiect_GC
     public partial class Form1 : Form
     {
         List<Point> Points = new List<Point>();
+        
         public Form1()
         {
             InitializeComponent();
@@ -21,9 +22,23 @@ namespace Proiect_GC
         private void Form1_MouseClick(object sender, MouseEventArgs e)
         {
             Point p = new Point(e.X, e.Y);
-            Points.Add(p);
 
-            label1.Text = "X = " + Points[0].X + "; Y = " + Points[0].Y;
+            if (Points.Count() == 5)
+            {
+                label1.Text = "X = " + Points[0].X + "; Y = " + Points[0].Y;
+                CreatePoligon();
+            }
+            else
+                Points.Add(p);
+        }
+
+        private void CreatePoligon()
+        {
+            Graphics gObject = this.CreateGraphics();
+            Pen pen = new Pen(Color.Black, 1);
+            if (Points.Count() == 5)
+                gObject.DrawLine(pen, Points[0], Points[1]);
+            gObject.Dispose();
         }
     }
 }
