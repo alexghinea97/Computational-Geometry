ea77fd6de253d4ea610ea2110f00d1c2067f3e5c
Alex Ghinea <alex.ghinea@equilobe.com>
2018-01-06 19:02:43 +0200

    init

---
+++ Proiect GC/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect_GC
+{
+    public partial class Form1 : Form
+    {
+        List<Point> Points = new List<Point>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point p = new Point(e.X, e.Y);
+            Points.Add(p);
+
+            label1.Text = "X = " + Points[0].X + "; Y = " + Points[0].Y;
+        }
+    }
+}
